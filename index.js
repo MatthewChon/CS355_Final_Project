cd7a59c925ba470c8d6354c1e4e70137e0cc7df6
Matthew Chon <matthewchon@icloud.com>
2021-12-20 11:59:30 -0500

    Implemented MapQuest API to project

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ function connection_handler(req, res) {
         res.writeHead(200, {"Content-Type":"text/html"});
         main.pipe(res);
     }
+    else if (req.url.startsWith("/map-photos/")) {
+        let image_stream = fs.createReadStream(`.${req.url}`);
+        image_stream.on("error", () => {
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.end(`404 Not Found`);
+        });
+        image_stream.on("ready", deliver_image);
+        function deliver_image() {
+            res.writeHead(200, {"Content-Type":"image/jpeg"});
+            image_stream.pipe(res);
+        }
+    }
     else if (req.url.startsWith("/search")) {
         const url = new URL(req.url, "https://localhost:3000");
         const pet_category = url.searchParams.get("category");
@@ -78,12 +90,12 @@ function stream_to_message(stream, callback, ...args) {
 function received_token(token_string, token_request_time, pet_category, user_location, res) {
     let token_object = JSON.parse(token_string);
     let access_token = token_object.access_token;
-    create_access_token_cache(token_object, token_request_time, pet_category, res);
+    create_access_token_cache(token_object, token_request_time, pet_category, user_location, res);
 }
 function create_access_token_cache(token_object, token_request_time, ...args) {
     token_object.expiration = new Date(token_request_time.getTime() + (token_object.expires_in * 1000));
     let access_token = token_object.access_token;
-    fs.writeFile('./auth/petfinder-token.json', JSON.stringify(token_object), () => {create_search_request(access_token, ...args)});
+    fs.writeFile('./auth/petfinder-token.json', JSON.stringify(token_object), () => create_search_request(access_token, ...args));
 }
 function create_search_request(access_token, pet_category, user_location, res) {
     const options = {
@@ -92,11 +104,11 @@ function create_search_request(access_token, pet_category, user_location, res) {
             "Authorization": `Bearer ${access_token}`
         }
     };
-    const search_query = querystring.stringify({type:pet_category});
+    const search_query = querystring.stringify({type:pet_category, location: user_location.state});
     const search_endpoint = `https://api.petfinder.com/v2/animals?${search_query}`;
     const search_request = https.request(search_endpoint, options);
     search_request.once("error", err => send_error_message(res));
-    search_request.once("response", search_result_stream => stream_to_message(search_result_stream, received_search_result, pet_category, res));
+    search_request.once("response", search_result_stream => stream_to_message(search_result_stream, received_search_result, pet_category, user_location, res));
     search_request.end();
 }
 function received_search_result(search_string, pet_category, user_location, res) {
@@ -104,22 +116,55 @@ function received_search_result(search_string, pet_category, user_location, res)
     const list_of_animals = search_object.animals.map(animal => animal);
 
     let animals = {animals_profile:[], max_size: list_of_animals.length};
-    list_of_animals.map((selected_animal) => process_information(selected_animal, animals, pet_category, res));
+    list_of_animals.map((selected_animal) => process_information(selected_animal, animals, pet_category, user_location, res));
 }
-function process_information(selected_animal, animals, pet_category, res) {
+function process_information(selected_animal, animals, pet_category, user_location, res) {
     let animal_location = Object.values(selected_animal.contact.address).map(address => address).join(" ");
+    let img = `map-photos/${selected_animal.name.split(' ').join('')}-${animal_location.split(' ').join('')}.jpg`;
     let animal_profile = {
         name: selected_animal.name,
         breed: selected_animal.breeds,
         age: selected_animal.age,
         gender: selected_animal.gender,
         url: selected_animal.url,
+        map_image: img,
         address: animal_location
     }
     animals.animals_profile.push(animal_profile);
-    if (animals.animals_profile.length >= animals.max_size) {
-        generate_webpage(animals, pet_category, res);
-    }
+    fs.access(img, fs.constants.R_OK, (err) => {
+        if (err) {
+            download_map_photo(animal_profile, animals, pet_category, user_location, res);
+        }
+        else {
+            console.log("Image is Cached");
+            if (animals.animals_profile.length >= animals.max_size) {
+                generate_webpage(animals, pet_category, res);
+            }
+        }
+    });
+}
+function download_map_photo(selected_animal, animals, pet_category, user_location, res) {
+    console.log(selected_animal);
+    const start_location = Object.values(user_location).map(address => address).join(" ");
+    const search_query = querystring.stringify({
+        key:api_key,
+        start: start_location,
+        end: selected_animal.address,
+        size: "200,200@2x"
+    });
+    const map_endpoint = `https://www.mapquestapi.com/staticmap/v5/map?${search_query}`;
+    const image_request = https.request(map_endpoint, {method: "GET"});
+    image_request.on("response", image_stream => {
+
+        const save_image = fs.createWriteStream(selected_animal.map_image, {'encoding': null});
+        image_stream.pipe(save_image);
+        save_image.on("finish", () => {
+            if (animals.animals_profile.length >= animals.max_size) {
+                generate_webpage(animals, pet_category, res);
+            }
+        });
+    });
+    image_request.end(search_query);
 }
 function generate_webpage(animals, pet_category, res) {
     animal_profiles = animals.animals_profile;
@@ -134,7 +179,8 @@ function generate_webpage(animals, pet_category, res) {
         else {
             structure += `Breed: ${information.breed.primary}<br>`;
         }
-        structure += `Address: ${information.address}</p>`;
+        structure += `Address: ${information.address}<br>
+        <img src="${information.map_image}" /></p>`
         return structure;
     }).join("");
     res.writeHead(200, {"Content-Type": "text/html"});
